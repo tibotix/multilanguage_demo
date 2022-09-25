e72ad77893543eb6912d7af6c2902a0fa08f0a44
tibotix <tizian@seehaus.net>
2022-09-25 02:51:50 +0200

    [fix] use result()

--- bindings/python/tests/unit/test_calculator.py
+++ bindings/python/tests/unit/test_calculator.py
@@ -3,25 +3,25 @@ import calculatorpy
 
 def test_calculator_add(calculator):
     calculator.add(5)
-    assert calculator.get() == 5.0
+    assert calculator.result() == 5.0
 
 def test_calculator_sub(calculator):
     calculator.sub(5)
-    assert calculator.get() == -5.0
+    assert calculator.result() == -5.0
 
 def test_calculator_mul(calculator):
     calculator.mul(5)
-    assert calculator.get() == 0.0
+    assert calculator.result() == 0.0
     calculator.add(5)
     calculator.mul(5)
-    assert calculator.get() == 25.0
+    assert calculator.result() == 25.0
 
 def test_calculator_div(calculator):
     calculator.div(5)
-    assert calculator.get() == 0.0
+    assert calculator.result() == 0.0
     calculator.add(25)
     calculator.div(5)
-    assert calculator.get() == 5.0
+    assert calculator.result() == 5.0
 
 def test_calculator_div_zero(calculator):
     with pytest.raises(calculatorpy.CalcError):
